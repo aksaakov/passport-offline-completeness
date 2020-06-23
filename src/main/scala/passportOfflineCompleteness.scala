90c3d3113a144a74fc7d51ae8f5f93d2ed5f3724
Ben-Wormald <benwormald95@gmail.com>
2020-06-23 09:19:22 +0100

    [DPUB-547] calculate passport completeness

--- src/main/scala/passportOfflineCompleteness.scala
+++ src/main/scala/passportOfflineCompleteness.scala
@@ -1,28 +1,43 @@
-import bbc.cps.vivostreamnotificationbridge.util.HttpConfig
-import main.scala.{Passport, Results, Teams}
+import bbc.dpub.PassportOfflineCompleteness.HttpConfig
+import bbc.dpub.PassportOfflineCompleteness.{Passport, Results, Teams, Weighting}
 import org.json4s.{DefaultFormats}
 import org.json4s.jackson.JsonMethods._
 
 import scala.io.Source._
 
-object passportOfflineCompleteness extends HttpConfig {
+object PassportOfflineCompleteness extends HttpConfig {
 
   implicit val formats = DefaultFormats
   val usedPredicates = List()
-  val weightingsJson = parse(fromResource("weightings").reader()).extract[Teams]
+  val weightingsJson = parse(fromResource("weightings.json").reader()).extract[Teams]
 
   def main(args: Array[String]) = {
+    val locator = "urn:bbc:test:bbcideas:partial"
+    val completeness = getCompleteness(getPassport(locator), weightingsJson)
+    println(s"Completeness of $locator = $completeness")
+  }
+
+  def getPassport(locator: String) = {
     def get(url: String) = fromURL(url).mkString
-    val url = "https://passport-store.int.api.bbci.co.uk/passport?locator=urn:bbc:cps:curie:asset:21312412"
-    val passport = parse(get(url)).extract[Results].results.head
-    getCompleteness(passport, weightingsJson)
+    val url = s"https://passport-store.int.api.bbci.co.uk/passport?locator=$locator"
+    parse(get(url)).extract[Results].results.head
   }
 
   def getCompleteness(passport: Passport, weightingsJson: Teams) = {
     val team = weightingsJson.teams.filter(team => team.teamName == passport.home).head
-    val weightSum = team.predicates.map(predicate => predicate.weight).sum
-    println(weightSum)
+    var passportPredicates = passport.taggings.map(tagging => tagging.predicate).distinct
+    if (!passport.language.isEmpty) passportPredicates = "Language" :: passportPredicates
+
+    val passportWeightSum = passportPredicates.map(predicate =>
+      team.predicates.find(weighting => weighting.predicate == predicate) match {
+        case Some(weighting) => weighting.weight
+        case None => {
+          println(s"No weighting defined for predicate $predicate")
+          0
+        }
+      }
+    ).sum
+    val totalWeightSum = team.predicates.map(predicate => predicate.weight).sum
+    passportWeightSum / totalWeightSum
   }
 }
-
-
